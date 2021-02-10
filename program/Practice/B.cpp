a61310f0fe1f328c1eba0837797328a886a2d6f6
lasjg28 <lassjg28@gmail.com>
2021-02-10 16:21:53 +0900

    remove directories except CoProgramming

--- program/Practice/B.cpp
+++
@@ -1,6 +0,0 @@
-using ll = long long;
-int tmp = 10;
-
-int calcB(int x, int y){
-    return x-y;
-}
--- program/Practice/Include.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef Include_hpp
-#define Include_hpp
-
-int calc(int x, int y){
-    return x+y;
-}
-
-#endif
